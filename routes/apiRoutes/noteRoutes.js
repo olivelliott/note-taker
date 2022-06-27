d6cc9c60600d498db8c36a180c2e2681b0e48c29
Olive <olivelliott48@gmail.com>
2022-06-27 00:10:58 -0400

    Create html routes and first GET api route to read json notes

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,10 +1,26 @@
-const path = require("path");
+// const path = require("path");
 const router = require("express").Router();
-const { notes } = require("../../db/db.json");
-// const notesHtml = require('../../public/index.html');
+const { filterByQuery } = require("../../lib/notes");
+const notes = require("../../db/db");
+// // const notesHtml = require('../../public/index.html');
+
+// // `POST /api/notes` should receive a new note to save on the request body, 
+// // add it to the `db.json` file, and then return the new note to the client. 
+// // You'll need to find a way to give each note a unique id when it's saved 
+// // (look into `npm` packages that could do this for you).
+
 
 router.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "../../public/notes.html"));
+    // let notesData = notes;
+    // if (req.query) {
+    //     notesData = filterByQuery(req.query, notesData);
+    // }
+    res.json(notes);
+    // res.send('hello')
 });
+
+// // router.post('/notes', (req, res) => {
+
+// // })
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const path = require("path");
+const nodemon = require('nodemon');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+const { notes } = require("./db/db");
+
 const apiRoutes = require("./routes/apiRoutes");
 // const htmlRoutes = require("./routes/htmlRoutes");
 
@@ -11,6 +16,19 @@ app.use(express.static("public"));
 
 app.use("/api", apiRoutes);
 // app.use("/", htmlRoutes);
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+})
+
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+})
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
