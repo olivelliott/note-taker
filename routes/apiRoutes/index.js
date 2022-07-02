52818296e9f1c2e344cd53b154ff62b44b0b86dd
Olive <olivelliott48@gmail.com>
2022-07-01 23:30:35 -0400

    Create post route to save a new note

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,8 +1,6 @@
 const router = require("express").Router();
 const noteRoutes = require("../apiRoutes/noteRoutes");
-const htmlRoutes = require("../htmlRoutes");
 
 router.use(noteRoutes);
-router.use(htmlRoutes);
 
 module.exports = router;
--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,7 +1,15 @@
 // const path = require("path");
 const router = require("express").Router();
-// const { filterByQuery } = require("../../lib/notes");
-const notes = require("../../db/db");
+// const {
+// //     // getNotes,
+//     saveNote
+// //     // deleteNote
+// } = require("../../public/assets/js/index");
+// const { saveNote } = require("../../public/assets/js/index")
+const noteData = require("../../db/db.json");
+const uuid = require("../../helpers/uuid");
+const { notes } = require('../../db/db.json');
+const { createNewNote } = require('../../lib/notes');
 
 // `POST /api/notes` should receive a new note to save on the request body, 
 // add it to the `db.json` file, and then return the new note to the client. 
@@ -10,15 +18,34 @@ const notes = require("../../db/db");
 
 
 router.get("/notes", (req, res) => {
-    res.json(notes);
+    res.json(noteData);
 });
 
-router.post("/notes", (req, res) => {
+router.post('/notes', (req, res) => {
+    req.body.id = uuid();
+    const { title, text } = req.body;
+    if (title && text) {
+        const newNotes = createNewNote(req.body, notes);
+        res.json(newNotes);
+        // const newNote = {
+        //     title,
+        //     text,
+        //     note_id: uuid(),
+        // };
+
+        // const response = {
+        //     status: "success",
+        //     body: newNote,
+        // };
+
+        // res.json(response);
+    } else {
+        res.status(400).send('The note is not properly formatted');
+    }
+});
+
+// router.post("/notes", (req, res) => {
     
-})
-
-// // router.post('/notes', (req, res) => {
-
-// // })
+// })
 
 module.exports = router;
