920548a73be9c659c7047a099f5b0d7d933364dd
Olive <olivelliott48@gmail.com>
2022-07-03 20:05:20 -0400

    Remove console logs & unnecessary code

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -14,11 +14,3 @@ router.get("*", (req, res) => {
 });
 
 module.exports = router;
-
-
-//  * DONE
-// GIVEN a note-taking application
-// WHEN I open the Note Taker
-// THEN I am presented with a landing page with a link to a notes page
-// WHEN I click on the link to the notes page
-// THEN I am presented with a page with existing notes listed in the left-hand column, plus empty fields to enter a new note title and the note’s text in the right-hand column
--- server.js
+++ server.js
@@ -1,25 +1,11 @@
-// WHEN I enter a new note title and the note’s text
-// THEN a Save icon appears in the navigation at the top of the page
-// WHEN I click on the Save icon
-// THEN the new note I have entered is saved and appears in the left-hand column with the other existing notes
-// WHEN I click on an existing note in the list in the left-hand column
-// THEN that note appears in the right-hand column
-// WHEN I click on the Write icon in the navigation at the top of the page
-// THEN I am presented with empty fields to enter a new note title and the note’s text in the right-hand column
-
-
-
 const express = require('express');
 const nodemon = require("nodemon");
-// const path = require("path");
 const apiRoutes = require("./routes/apiRoutes");
 const htmlRoutes = require("./routes/htmlRoutes");
 
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-
-// const { notes } = require("./db/db");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
